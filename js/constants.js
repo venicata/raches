c3f22dc079bafc57cf47cad5a5ed0c287de6fdd3
Venelin Venelinov <veniaminikus@gmail.com>
2025-09-10 14:20:30 +0300

    changes

--- js/constants.js
+++ js/constants.js
@@ -4,6 +4,9 @@ export const KNOTS_TO_MS = 0.514444;
 export const RACHES_LAT = 38.867085;
 export const RACHES_LON = 22.759371;
 
+export const LAMIA_LAT = 38.902790;
+export const LAMIA_LON = 22.443008;
+
 // Координати за Волос за температурата на водата
 export const VOLOS_LAT = 38.9534;
 export const VOLOS_LON = 22.9668;
